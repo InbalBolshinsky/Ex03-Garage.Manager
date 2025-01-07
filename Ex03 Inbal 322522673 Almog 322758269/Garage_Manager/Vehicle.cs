ff7cc65129be8600a551ca20a2b100c3fac4db84
InbalBolshinsky <Inbalbolsh@gmail.com>
2025-01-07 13:34:45 +0200

    initializing classes

--- Ex03 Inbal 322522673 Almog 322758269/Garage_Manager/Vehicle.cs
+++ Ex03 Inbal 322522673 Almog 322758269/Garage_Manager/Vehicle.cs
@@ -2,11 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Garage_Manager
 {
-    internal class Vehicle
+    public abstract class Vehicle
     {
+        //- m_ModelName (string)
+        //- m_LicenseNumber (string)
+        //- m_RemainingEnergy (float)
+        //- m_Wheel<T>:
+
+        private readonly string m_ModelName;
+        private readonly string m_LicenseNumber;
+        private float m_RemainingEnergy;
+        private Wheel[] m_Wheels;
+
+
+        public Vehicle(int i_NumberOfWheels, float i_RemainingEnergy)
+        {
+            Wheel[] m_Wheels = new Wheel[i_NumberOfWheels];
+            for (int i = 0; i < i_NumberOfWheels; i++)
+            {
+                m_Wheels[i] = new Wheel(i_RemainingEnergy);
+            }
+
+        }
+
+        public string ModelName
+        {
+            get
+            {
+                return m_ModelName;
+            }
+        }
+
+        public string LicenseNumber
+        {
+            get
+            {
+                return m_LicenseNumber;
+            }
+        }
+
+        public float RemainingEnergy
+        {
+            get
+            {
+                return m_RemainingEnergy;
+            }
+            set
+            {
+                m_RemainingEnergy = value;
+            }
+        }
+
+         
     }
 }
---
+++ Ex03 Inbal 322522673 Almog 322758269/Garage_Manager/Wheel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Manager
+{
+    public class Wheel
+    {
+        //- m_Manufacturer(string)
+        //- m_CurrentAirPressure(float)
+        //-m_MaxAirPressure(float)
+
+        //+ (return value) Pump(i_PressureToAdd)
+
+        private string m_manufacturer;
+        private float m_currentAirPressure;
+        private readonly float m_maxAirPressure;
+
+        public Wheel(float i_maxAirPressure)
+        {
+            m_maxAirPressure = i_maxAirPressure;
+        }
+
+        public float MaxAirPressure
+        {
+            get
+            {
+                return m_maxAirPressure;
+            }
+            set
+            {
+                m_maxAirPressure = value;
+            }
+        }
+
+        public float CurrentAirPressure
+        {
+            get
+            {
+                return m_currentAirPressure;
+            }
+            set
+            {
+                if (value > MaxAirPressure)
+                {
+                    throw new ValueOutOfRangeException(0, MaxAirPressure);
+                }
+
+                m_currentAirPressure = value;
+            }
+        }
+        }
+
+
+
+    }
+}
