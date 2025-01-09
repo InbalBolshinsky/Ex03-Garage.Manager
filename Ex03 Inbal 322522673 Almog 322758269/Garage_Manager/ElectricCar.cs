d0321b481d66bdbe7c5afdaaf934c2d25c187999
InbalBolshinsky <Inbalbolsh@gmail.com>
2025-01-09 11:19:05 +0200

    commit

---
+++ Ex03 Inbal 322522673 Almog 322758269/Garage_Manager/ElectricCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class ElectricCar : ElectricVehicle
+    {
+        private eCarColors color;
+        private eNumberOfDoors doors;
+
+        public ElectricCar(string i_LicenseNumber, string i_Model, float i_RemainingBatteryTime, eCarColors i_Color, eCarDoors i_Doors) : base(i_LicenseNumber, i_Model, i_RemainingBatteryTime)
+        {
+            color = i_Color;
+            doors = i_Doors;
+        }
+
+
+
+    }
+}
